b50404080a28211f691864eae21ec72f11b24e41
reptilecollective <145528872+reptilecollective@users.noreply.github.com>
2024-01-26 00:13:00 -0500

    Fixed auto carousel, converted WEBPs to PNGs

--- script.js
+++ script.js
@@ -19,46 +19,18 @@ document.querySelectorAll('.carousel').forEach(carousel => {
     const allDots = Array.from(carousel.querySelectorAll('.dot'));
     let timer;
     
-    /** Get current image number (starts at 0) */
-    function getIndex() {
-        let width = carousel.offsetWidth;
-        let pos = slider.scrollLeft;
-        let index = Math.round(pos/width);
-        return index;
-    }
+    /** Get current image number (starts at 0). Position divided by width */
+    function getIndex() { return Math.round(slider.scrollLeft/carousel.offsetWidth); }
 
-    function currentImage() { return carousel.querySelector(`[data-image="${getIndex()}"]`); }
     /** Scroll image container */
     function goto(x=0) { slider.scrollTo({left:x, behavior:'smooth'}); }
 
-    // Scrollable area
-    slider.addEventListener('scroll', e => {
-        for(let d of allDots) d.classList.remove('active');
-        allDots[getIndex()].classList.add('active');
-        background_image.style = `background: linear-gradient(0deg, rgb(20, 11, 11) 0%, rgba(5, 5, 5, 0.8) 100%), url(${currentImage()?.src ?? 'assets/reptile_egg_incubation.jpg'})`;
-        background_image.classList.remove('slidein');
-        void background_image.offsetWidth;
-        background_image.classList.add('slidein');
-    });
-
-    // Dots
-    for(let i in allDots) {
-        let element = allDots[i];
-        element.addEventListener('click', event => {
-            goto(slider.offsetWidth*Number(i));
-        })
-    }
-
-    /** Set auto timer */
-    function setTimer() {
-        if(!carousel.dataset.timer) return;
-        clearInterval(timer);
-        timer = setInterval(() => {cycleImage(undefined, true);}, Number(carousel.dataset.timer));
-    }
+    /** Returns visible element */
+    function currentSlide() { return carousel.querySelector(`[data-image="${getIndex()}"]`); }
 
     /** Cycles to next or previous image */
     function cycleImage(event, auto) {
-        let dir = Number(event?.srcElement.dataset?.direction) ?? 1;
+        let dir = Number(event?.srcElement.dataset?.direction) || 1;
         let end = allDots.length * slider.offsetWidth - slider.offsetWidth;
         let at_end = slider.scrollLeft >= end && dir === 1; // Start over when at the end
         let at_start = slider.scrollLeft === 0 && dir === -1; // Skip to end when at the start
@@ -67,8 +39,34 @@ document.querySelectorAll('.carousel').forEach(carousel => {
         if(!auto) setTimer(); // Reset timer
     }
 
-    // Buttons
+    // Scroll event
+    slider.addEventListener('scroll', e => {
+        let index = getIndex();
+        for(let d of allDots) d.classList.remove('active');
+        allDots[index].classList.add('active');
+
+        // Update background
+        background_image.style = `background: linear-gradient(0deg, rgb(20, 11, 11) 0%, rgba(5, 5, 5, 0.8) 100%), url(${currentSlide()?.src ?? 'assets/reptile_egg_incubation.jpg'})`;
+        background_image.classList.remove('slidein');
+        void background_image.offsetWidth;
+        background_image.classList.add('slidein');
+    });
+
+    // Next/previous buttons
     carousel.querySelectorAll(".arrow").forEach(e => e.addEventListener('click', cycleImage));
+
+    // Dot onclick
+    for(let i in allDots) {
+        let element = allDots[i];
+        element.addEventListener('click', event => goto(slider.offsetWidth*Number(i)));
+    }
+
+    /** Set auto timer */
+    function setTimer() {
+        if(!carousel.dataset.timer) return;
+        clearInterval(timer);
+        timer = setInterval(() => {cycleImage(undefined, true);}, Number(carousel.dataset.timer));
+    }
 
     // Auto
     setTimer();
